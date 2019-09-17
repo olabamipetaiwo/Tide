2c0d58db30912fefe9f8d727b7a9a3e42375fcf4
Olabamipe Taiwo <teeola48@gmail.com>
2019-09-17 03:53:23 +0000

    Updates

---
+++ precache-manifest.8561b0464dd34e91bedec511d695f724.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eeeabb150f21bac50d0125e5def6b1f",
+    "url": "/tide/index.html"
+  },
+  {
+    "revision": "529700b6e3f01fd00682",
+    "url": "/tide/static/css/main.79cd2a08.chunk.css"
+  },
+  {
+    "revision": "e8ebc0a1388546cb30c5",
+    "url": "/tide/static/js/2.707d7af9.chunk.js"
+  },
+  {
+    "revision": "529700b6e3f01fd00682",
+    "url": "/tide/static/js/main.9f0be332.chunk.js"
+  },
+  {
+    "revision": "9eba7d27254e2c2543e9",
+    "url": "/tide/static/js/runtime~main.882ce3f1.js"
+  }
+]);
