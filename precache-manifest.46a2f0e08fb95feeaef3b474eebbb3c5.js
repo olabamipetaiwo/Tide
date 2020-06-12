9905564e3e6dbd974ee2f6e79fa2cfee54f7966b
Olabamipe Taiwo <teeola48@gmail.com>
2020-06-12 02:41:59 +0000

    Updates

---
+++ precache-manifest.46a2f0e08fb95feeaef3b474eebbb3c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0164bbabc0db24b54ab3e44b905a7451",
+    "url": "/tide/index.html"
+  },
+  {
+    "revision": "25d7e58fe6cc15f094e7",
+    "url": "/tide/static/css/main.79cd2a08.chunk.css"
+  },
+  {
+    "revision": "b993cd1f1480523895bc",
+    "url": "/tide/static/js/2.95c243c5.chunk.js"
+  },
+  {
+    "revision": "25d7e58fe6cc15f094e7",
+    "url": "/tide/static/js/main.0f8b376c.chunk.js"
+  },
+  {
+    "revision": "1cdc950f0a0adf0eecfc",
+    "url": "/tide/static/js/runtime~main.f2bfd37b.js"
+  }
+]);
